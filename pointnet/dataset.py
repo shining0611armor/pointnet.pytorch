0de62b31ecdd316977c68391dcf8ad69be30acbd
Mehran Tamjidi <76002206+shining0611armor@users.noreply.github.com>
2024-08-23 20:30:49 +0330

    Update dataset.py

--- pointnet/dataset.py
+++ pointnet/dataset.py
@@ -9,6 +9,21 @@ from tqdm import tqdm
 import json
 from plyfile import PlyData, PlyElement
 
+
+import torch.utils.data as data
+import os
+import os.path
+import torch
+import numpy as np
+import sys
+from tqdm import tqdm
+from torch.utils.data import Dataset
+from plyfile import PlyData, PlyElement
+import glob
+import h5py
+import torch.utils.data
+
+
 def get_segmentation_classes(root):
     catfile = os.path.join(root, 'synsetoffset2category.txt')
     cat = {}
@@ -140,43 +155,75 @@ class ShapeNetDataset(data.Dataset):
     def __len__(self):
         return len(self.datapath)
 
-class ModelNetDataset(data.Dataset):
-    def __init__(self,
-                 root,
-                 npoints=2500,
-                 split='train',
-                 data_augmentation=True):
+def download(root):
+    if not os.path.exists(root):
+        os.mkdir(root)
+    if not os.path.exists(os.path.join(root, 'modelnet40_ply_hdf5_2048')):
+        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+        zipfile = os.path.basename(www)
+        os.system('wget --no-check-certificate %s; unzip %s' % (www, zipfile))
+        os.system('mv %s %s' % (zipfile[:-4], root))
+        os.system('rm %s' % (zipfile))
+
+# download('/content/pointnet.pytorch')
+
+def load_data(root, partition):
+    root = os.path.join(root, 'data')
+    download(root)
+    all_data = []
+    all_label = []
+    g = sorted(glob.glob(os.path.join(root, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5' % partition)))
+    for h5_name in g:
+        f = h5py.File(h5_name)
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+        # print(all_data)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+
+# Use the load_data function
+root_dir = '/content/pointnet.pytorch'
+partition = 'train'  # or 'test'
+
+data, labels = load_data(root_dir, partition)
+
+# Verify the data
+print(f'Data shape: {data.shape}')
+print(f'Labels shape: {labels.shape}')
+
+
+
+class ModelNetDataset(Dataset):
+    def __init__(self, root, npoints=2500, split='train', data_augmentation=True):
         self.npoints = npoints
-        self.root = root
-        self.split = split
         self.data_augmentation = data_augmentation
-        self.fns = []
-        with open(os.path.join(root, '{}.txt'.format(self.split)), 'r') as f:
-            for line in f:
-                self.fns.append(line.strip())
+        self.split = split
 
-        self.cat = {}
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../misc/modelnet_id.txt'), 'r') as f:
-            for line in f:
-                ls = line.strip().split()
-                self.cat[ls[0]] = int(ls[1])
+        # Load data and labels using the load_data function
+        self.data, self.labels = load_data(root, split)
 
-        print(self.cat)
+        # Map labels to classes (This assumes you have a mapping of labels to class names)
+        self.cat = {i: i for i in range(40)}  # Assuming 40 classes as in ModelNet40 dataset
         self.classes = list(self.cat.keys())
 
     def __getitem__(self, index):
-        fn = self.fns[index]
-        cls = self.cat[fn.split('/')[0]]
-        with open(os.path.join(self.root, fn), 'rb') as f:
-            plydata = PlyData.read(f)
-        pts = np.vstack([plydata['vertex']['x'], plydata['vertex']['y'], plydata['vertex']['z']]).T
-        choice = np.random.choice(len(pts), self.npoints, replace=True)
-        point_set = pts[choice, :]
+        point_set = self.data[index]
+        cls = self.labels[index]
 
+        # Randomly select points (if npoints < 2048)
+        choice = np.random.choice(point_set.shape[0], self.npoints, replace=True)
+        point_set = point_set[choice, :]
+
+        # Center and normalize the point set
         point_set = point_set - np.expand_dims(np.mean(point_set, axis=0), 0)  # center
         dist = np.max(np.sqrt(np.sum(point_set ** 2, axis=1)), 0)
         point_set = point_set / dist  # scale
 
+        # Apply data augmentation if specified
         if self.data_augmentation:
             theta = np.random.uniform(0, np.pi * 2)
             rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
@@ -187,9 +234,8 @@ class ModelNetDataset(data.Dataset):
         cls = torch.from_numpy(np.array([cls]).astype(np.int64))
         return point_set, cls
 
-
     def __len__(self):
-        return len(self.fns)
+        return len(self.data)
 
 if __name__ == '__main__':
     dataset = sys.argv[1]
