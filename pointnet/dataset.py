be6101f72274605ed9bed3b64782214784a03161
Mehran Tamjidi <76002206+shining0611armor@users.noreply.github.com>
2024-08-25 01:17:44 +0330

    Update dataset.py

--- pointnet/dataset.py
+++ pointnet/dataset.py
@@ -6,156 +6,74 @@ import torch
 import numpy as np
 import sys
 from tqdm import tqdm 
-import json
-from plyfile import PlyData, PlyElement
-
-
-import torch.utils.data as data
-import os
-import os.path
-import torch
-import numpy as np
-import sys
-from tqdm import tqdm
 from torch.utils.data import Dataset
-from plyfile import PlyData, PlyElement
 import glob
 import h5py
 import torch.utils.data
+import requests
+import zipfile
 
 
-def get_segmentation_classes(root):
-    catfile = os.path.join(root, 'synsetoffset2category.txt')
-    cat = {}
-    meta = {}
+#----------------------------shapenet dataset-------------------------
 
-    with open(catfile, 'r') as f:
-        for line in f:
-            ls = line.strip().split()
-            cat[ls[0]] = ls[1]
-
-    for item in cat:
-        dir_seg = os.path.join(root, cat[item], 'points_label')
-        dir_point = os.path.join(root, cat[item], 'points')
-        fns = sorted(os.listdir(dir_point))
-        meta[item] = []
-        for fn in fns:
-            token = (os.path.splitext(os.path.basename(fn))[0])
-            meta[item].append((os.path.join(dir_point, token + '.pts'), os.path.join(dir_seg, token + '.seg')))
+def download_shapenet(root):
+    # Ensure the root directory exists
+    if not os.path.exists(root):
+        os.mkdir(root)
     
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../misc/num_seg_classes.txt'), 'w') as f:
-        for item in cat:
-            datapath = []
-            num_seg_classes = 0
-            for fn in meta[item]:
-                datapath.append((item, fn[0], fn[1]))
-
-            for i in tqdm(range(len(datapath))):
-                l = len(np.unique(np.loadtxt(datapath[i][-1]).astype(np.uint8)))
-                if l > num_seg_classes:
-                    num_seg_classes = l
-
-            print("category {} num segmentation classes {}".format(item, num_seg_classes))
-            f.write("{}\t{}\n".format(item, num_seg_classes))
-
-def gen_modelnet_id(root):
-    classes = []
-    with open(os.path.join(root, 'train.txt'), 'r') as f:
-        for line in f:
-            classes.append(line.strip().split('/')[0])
-    classes = np.unique(classes)
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../misc/modelnet_id.txt'), 'w') as f:
-        for i in range(len(classes)):
-            f.write('{}\t{}\n'.format(classes[i], i))
-
-class ShapeNetDataset(data.Dataset):
-    def __init__(self,
-                 root,
-                 npoints=2500,
-                 classification=False,
-                 class_choice=None,
-                 split='train',
-                 data_augmentation=True):
-        self.npoints = npoints
-        self.root = root
-        self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')
-        self.cat = {}
-        self.data_augmentation = data_augmentation
-        self.classification = classification
-        self.seg_classes = {}
-        
-        with open(self.catfile, 'r') as f:
-            for line in f:
-                ls = line.strip().split()
-                self.cat[ls[0]] = ls[1]
-        #print(self.cat)
-        if not class_choice is None:
-            self.cat = {k: v for k, v in self.cat.items() if k in class_choice}
-
-        self.id2cat = {v: k for k, v in self.cat.items()}
-
-        self.meta = {}
-        splitfile = os.path.join(self.root, 'train_test_split', 'shuffled_{}_file_list.json'.format(split))
-        #from IPython import embed; embed()
-        filelist = json.load(open(splitfile, 'r'))
-        for item in self.cat:
-            self.meta[item] = []
-
-        for file in filelist:
-            _, category, uuid = file.split('/')
-            if category in self.cat.values():
-                self.meta[self.id2cat[category]].append((os.path.join(self.root, category, 'points', uuid+'.pts'),
-                                        os.path.join(self.root, category, 'points_label', uuid+'.seg')))
-
-        self.datapath = []
-        for item in self.cat:
-            for fn in self.meta[item]:
-                self.datapath.append((item, fn[0], fn[1]))
-
-        self.classes = dict(zip(sorted(self.cat), range(len(self.cat))))
-        print(self.classes)
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../misc/num_seg_classes.txt'), 'r') as f:
-            for line in f:
-                ls = line.strip().split()
-                self.seg_classes[ls[0]] = int(ls[1])
-        self.num_seg_classes = self.seg_classes[list(self.cat.keys())[0]]
-        print(self.seg_classes, self.num_seg_classes)
-
-    def __getitem__(self, index):
-        fn = self.datapath[index]
-        cls = self.classes[self.datapath[index][0]]
-        point_set = np.loadtxt(fn[1]).astype(np.float32)
-        seg = np.loadtxt(fn[2]).astype(np.int64)
-        #print(point_set.shape, seg.shape)
-
-        choice = np.random.choice(len(seg), self.npoints, replace=True)
-        #resample
-        point_set = point_set[choice, :]
-
-        point_set = point_set - np.expand_dims(np.mean(point_set, axis = 0), 0) # center
-        dist = np.max(np.sqrt(np.sum(point_set ** 2, axis = 1)),0)
-        point_set = point_set / dist #scale
-
-        if self.data_augmentation:
-            theta = np.random.uniform(0,np.pi*2)
-            rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]])
-            point_set[:,[0,2]] = point_set[:,[0,2]].dot(rotation_matrix) # random rotation
-            point_set += np.random.normal(0, 0.02, size=point_set.shape) # random jitter
-
-        seg = seg[choice]
-        point_set = torch.from_numpy(point_set)
-        seg = torch.from_numpy(seg)
-        cls = torch.from_numpy(np.array([cls]).astype(np.int64))
-
-        if self.classification:
-            return point_set, cls
+    #Check if the dataset directory already exists
+    dataset_dir = os.path.join(root, 'shapenetcorev2_hdf5_2048')
+    if not os.path.exists(dataset_dir):
+        url = 'https://cloud.tsinghua.edu.cn/f/06a3c383dc474179b97d/?dl=1'
+        local_zip_file = os.path.join(root, 'shapenetcorev2_hdf5_2048.zip')
+        print("Downloading ZIP file...")
+        # Download the ZIP file
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the download was successful
+        #  Save the ZIP file to the specified directory
+        with open(local_zip_file, 'wb') as f:
+            f.write(response.content)
+        print("ZIP file downloaded successfully!")
+        #  Extract the ZIP file
+        print("Extracting ZIP file...")
+        with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
+            zip_ref.extractall(root)  # Extract contents into the root directory
+        print("ZIP file extracted successfully!")
+        #  Move the extracted files to the dataset directory
+        extracted_folder = os.path.join(root, 'shapenetcorev2_hdf5_2048')
+        if not os.path.exists(dataset_dir):
+            os.rename(extracted_folder, dataset_dir)
         else:
-            return point_set, seg
+            print("Directory already exists. Skipping move.")
+        #  Clean up by removing the downloaded ZIP file
+        os.remove(local_zip_file)
+        print("Downloaded ZIP file removed.")
+    else:
+        print("Dataset already exists. No download needed.")
 
-    def __len__(self):
-        return len(self.datapath)
 
-def download(root):
+def load_data_shapenet(root, partition):
+    root = os.path.join(root, 'data')
+    download_shapenet(root)
+    all_data = []
+    all_label = []
+    g = sorted(glob.glob(os.path.join(root, 'shapenetcorev2_hdf5_2048', '%s*.h5' % partition)))
+    for h5_name in g:
+        f = h5py.File(h5_name)
+        data = f['data'][:].astype('float32')
+        label = f['label'][:].astype('int64')
+        f.close()
+        all_data.append(data)
+        all_label.append(label)
+        #print(all_data)
+    all_data = np.concatenate(all_data, axis=0)
+    all_label = np.concatenate(all_label, axis=0)
+    return all_data, all_label
+
+
+#-------------------------------ModelNet Dataset-----------------------
+
+def download_modelnet(root):
     if not os.path.exists(root):
         os.mkdir(root)
     if not os.path.exists(os.path.join(root, 'modelnet40_ply_hdf5_2048')):
@@ -167,9 +85,9 @@ def download(root):
 
 # download('/content/pointnet.pytorch')
 
-def load_data(root, partition):
+def load_data_modelnet(root, partition):
     root = os.path.join(root, 'data')
-    download(root)
+    download_modelnet(root)
     all_data = []
     all_label = []
     g = sorted(glob.glob(os.path.join(root, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5' % partition)))
@@ -184,30 +102,37 @@ def load_data(root, partition):
     all_data = np.concatenate(all_data, axis=0)
     all_label = np.concatenate(all_label, axis=0)
     return all_data, all_label
-
+'''
 # Use the load_data function
 root_dir = '/content/pointnet.pytorch'
 partition = 'train'  # or 'test'
 
-data, labels = load_data(root_dir, partition)
+data, labels = load_data_shapenet(root_dir, partition)
 
 # Verify the data
 print(f'Data shape: {data.shape}')
 print(f'Labels shape: {labels.shape}')
+'''
+#-----------------------------------Dataset class for all datasets---------------------- 
 
-
-
-class ModelNetDataset(Dataset):
-    def __init__(self, root, npoints=2500, split='train', data_augmentation=True):
+class DatasetClass(Dataset):
+    def __init__(self, root, dataset_name='modelnet' , npoints=2500, split='train', data_augmentation=True):
         self.npoints = npoints
         self.data_augmentation = data_augmentation
         self.split = split
+        self.dataset_name = dataset_name
+        self.data, self.labels = None, None
+        
+        if self.dataset_name.lower() == 'modelnet':
+            self.data, self.labels = load_data_modelnet(root, split)
+            self.cat = {i: i for i in range(40)}  # ModelNet40 has 40 classes
+        elif self.dataset_name.lower() == 'shapenet':
+            self.data, self.labels = load_data_shapenet(root, split)
+            self.cat = {i: i for i in range(55)}  # ShapeNet has 55 classes
+        else:
+            raise ValueError(f"Unsupported dataset: {self.dataset_name}. Choose 'modelnet' or 'shapenet'.")
 
-        # Load data and labels using the load_data function
-        self.data, self.labels = load_data(root, split)
-
-        # Map labels to classes (This assumes you have a mapping of labels to class names)
-        self.cat = {i: i for i in range(40)}  # Assuming 40 classes as in ModelNet40 dataset
+        # List of class indices
         self.classes = list(self.cat.keys())
 
     def __getitem__(self, index):
@@ -237,25 +162,22 @@ class ModelNetDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+
+
+
+
+
 if __name__ == '__main__':
     dataset = sys.argv[1]
     datapath = sys.argv[2]
 
     if dataset == 'shapenet':
-        d = ShapeNetDataset(root = datapath, class_choice = ['Chair'])
+        d = DatasetClass(root=datapath, dataset_name = dataset)
         print(len(d))
-        ps, seg = d[0]
-        print(ps.size(), ps.type(), seg.size(),seg.type())
-
-        d = ShapeNetDataset(root = datapath, classification = True)
-        print(len(d))
-        ps, cls = d[0]
-        print(ps.size(), ps.type(), cls.size(),cls.type())
-        # get_segmentation_classes(datapath)
+        print(d[0])
 
     if dataset == 'modelnet':
-        gen_modelnet_id(datapath)
-        d = ModelNetDataset(root=datapath)
+        d = DatasetClass(root=datapath, dataset_name = dataset)
         print(len(d))
         print(d[0])
 
